4f06c4f2786a6d3fd98e20a5488c50f074a7303c
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2021-04-18 16:31:09 +0500

    Challenge complete
    Pricing section with toggle to switch between annual and monthly rates

---
+++ pricing-component-with-billing-toggle/app.js
@@ -0,0 +1,25 @@
+const toggle = document.querySelector(".js-toggle-checkbox")
+const priceElems = document.querySelectorAll(".js-card-price")
+
+const annualPrices = [199.99, 249.99, 399.99]
+const monthlyPrices = [19.99, 24.99, 39.99]
+
+const getPrices = () => {
+    if (toggle.checked) {
+        return monthlyPrices
+    } else {
+        return annualPrices
+    }
+}
+
+const updateElems = () => {
+    newPrices = getPrices()
+
+    for (let p = 0; p < priceElems.length; p++) {
+        priceElems[p].textContent =  newPrices[p]
+    } 
+}
+
+
+updateElems()
+toggle.addEventListener("click", updateElems)
