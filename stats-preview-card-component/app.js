1a2e286716f05eb7790ce9e10fa1e477c7be6dc3
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2021-04-22 00:12:51 +0500

    Challenge complete!
    Stats preview card component with 3D hover effect

---
+++ stats-preview-card-component/app.js
@@ -0,0 +1,26 @@
+wrapper = document.querySelector(".wrapper")
+card = document.querySelector(".card")
+
+wrapper.addEventListener("mousemove", function (e) {
+    x = e.clientX
+    y = e.clientY
+
+    w = getComputedStyle(wrapper).width.slice(0,-2)
+    h = getComputedStyle(wrapper).height.slice(0,-2)
+
+    xPercent = (x / w) - 0.5
+    yPercent = (y / h) - 0.5
+
+    // console.log(xPercent, yPercent);
+
+    rotateY = xPercent * 30
+    rotateX = -(yPercent * 30)
+
+
+    // console.log(rotateX, rotateY);
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+})
+
+wrapper.addEventListener("mouseout", function () {
+    card.style.transform = "rotateX(0) rotateY(0)"
+})
