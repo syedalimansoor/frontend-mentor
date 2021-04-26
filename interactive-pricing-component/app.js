848c7e92c4a56e4b813554f41f1a9a459b050cd0
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2021-04-26 16:41:25 +0500

    Challenge complete
    Interactive pricing component

---
+++ interactive-pricing-component/app.js
@@ -0,0 +1,31 @@
+slider = document.querySelector(".js-card-slider")
+toggle = document.querySelector(".js-card-toggle")
+priceElem = document.querySelector(".js-card-price")
+pageviewsElem = document.querySelector(".js-card-pageviews")
+
+pageviews = ["10K", "50K", "100K", "500K", "1M"]
+prices = [8, 12, 16, 24, 36]
+
+function getPrices() {
+    if (toggle.checked) {
+        return prices.map(p => p * 0.75)
+    } else {
+        return prices
+    }
+}
+
+function getPrice() {
+    return getPrices()[parseInt(slider.value)-1]
+}
+
+function getPageviews() {
+    return pageviews[parseInt(slider.value) - 1]
+}
+
+function updateElems() {
+    priceElem.innerText = `$${getPrice()}.00`
+    pageviewsElem.innerText = getPageviews()
+}
+
+slider.addEventListener("input", updateElems)
+toggle.addEventListener("input", updateElems)
