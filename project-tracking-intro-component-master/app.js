75b4994b136a244a9434a4b7f104153a760f31d1
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2021-05-26 22:36:05 +0500

    Commit

--- project-tracking-intro-component-master/app.js
+++ project-tracking-intro-component-master/app.js
@@ -3,10 +3,10 @@ const navIcon = document.querySelector(".nav-toggle__img");
 const nav = document.querySelector(".page__nav");
 
 const toggleNav = function () {
-  nav.classList.add("transition");
-  nav.clientWidth;
+  // nav.classList.add("transition");
+  // nav.clientWidth;
   nav.classList.toggle("open");
-  nav.classList.remove("transition");
+  // nav.classList.remove("transition");
   nav.classList.contains("open")
     ? (navIcon.src = "./images/icon-close.svg")
     : (navIcon.src = "./images/icon-hamburger.svg");
