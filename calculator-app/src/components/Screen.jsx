1cd5188501dccfc6afc02bedae8d9f4f585c99fb
Syed Ali Mansoor <76787451+alimansoor-create@users.noreply.github.com>
2022-02-02 00:39:18 +0500

    removed unused import

--- calculator-app/src/components/Screen.jsx
+++ calculator-app/src/components/Screen.jsx
@@ -1,5 +1,4 @@
 import { useContext } from "react";
-import { Textfit } from "react-textfit";
 import styled from "styled-components";
 
 import CalculatorContext from "../context/CalculatorContext";
